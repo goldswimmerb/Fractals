75e791725f449926085c43a3937593aa50e60778
goldswimmerb <goldswimmerbj@gmail.com>
2015-03-19 08:39:20 -0400

    Now this draws a 21 tree

--- src/Trees/FractalTree.java
+++ src/Trees/FractalTree.java
@@ -12,17 +12,18 @@ public class FractalTree extends JPanel {
 //Recursive Method
 	private void drawTree(Graphics g, int x1, int y1, double angle, int depth){
 		if(depth ==0){return;}
-		int x2 = x1+(int)(Math.cos(Math.toRadians(angle))* depth * 10);//left side
-		int y2 = y1+(int)(Math.sin(Math.toRadians(angle))* depth * 10);//right side
+		int x2 = x1+(int)(Math.cos(Math.toRadians(angle))* depth * 5);//left side
+		int y2 = y1+(int)(Math.sin(Math.toRadians(angle))* depth * 5);//right side
 		g.drawLine(x1,  y1,  x2,  y2);//base of tree
-		drawTree(g, x2 ,y2, angle -35, depth -1); //Left side of tree
-		drawTree(g, x2, y2, angle+35, depth -1);//right side of tree
+		drawTree(g, x2 ,y2, angle -30, depth -1); //Left side of tree
+		drawTree(g, x2, y2, angle+ 30, depth -1);//right side of tree
 
 	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.black); //tree color
-		drawTree(g, 200, 300, -90, 6); //xPos, yPos, vertical, levels
+		//drawTree(g, 250, 400, -90, 15); //xPos, yPos, vertical, levels
+		drawTree(g, 960, 540, -90, 21); //xPos, yPos, vertical, levels
 	}
 }
--- src/Trees/FractalTreeRunner.java
+++ src/Trees/FractalTreeRunner.java
@@ -4,8 +4,8 @@ import javax.swing.JPanel;
 
 public class FractalTreeRunner extends JApplet {
 	
-	private final int A_W = 2000; // Width of window
-	private final int A_H = 2000; //Height of window
+	private final int A_W = 1920; // Width of window
+	private final int A_H = 1080; //Height of window
 	
 	private FractalTree graphic; //Instance of Fractal
 	private JPanel appletPanel;// instance of internal Panel
