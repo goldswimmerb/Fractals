967faafafeb688d2b268c2a173b6b8db785347a8
goldswimmerb <goldswimmerbj@gmail.com>
2015-03-19 08:18:11 -0400

    FractalTree now draws panel

--- src/Trees/FractalTree.java
+++ src/Trees/FractalTree.java
@@ -1,5 +1,13 @@
 package Trees;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel;
 
-public class FractalTree {
-
+public class FractalTree extends JPanel {
+	public FractalTree(int PANEL_WIDTH,int PANEL_HEIGHT ){
+		setBackground(Color.white);
+		setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+	}
+//Recursive Method
 }
